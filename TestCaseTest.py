408d74720f461cc1bea5587268ca2a2df1a95d1c
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-08-07 01:17:37 +0900

    Remove testRunning and rename testSetUp to testTemplateMethod

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -4,14 +4,9 @@ from WasRun import WasRun
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
-        
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
 
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert ("setUp testMethod " == self.test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
