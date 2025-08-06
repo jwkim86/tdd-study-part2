662c351ebd8756e336fc5030a36553b19216ec7b
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-08-07 01:32:41 +0900

    Add test for test result

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -7,4 +7,9 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert ("setUp testMethod tearDown " == self.test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
