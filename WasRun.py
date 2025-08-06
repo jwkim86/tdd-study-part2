a17d0249420e148b5404e6959496d3b67ea533c0
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-08-07 01:24:56 +0900

    Add tearDown

--- WasRun.py
+++ WasRun.py
@@ -2,13 +2,14 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
     def setUp(self):
-        self.wasRun = None
-        self.log= "setUp "
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
