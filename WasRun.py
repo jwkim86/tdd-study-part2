d6f11c86ac5fc1180b4bb99cfc71daa549ca7fa4
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-08-07 00:30:04 +0900

    Move run method to TestCase class

--- WasRun.py
+++ WasRun.py
@@ -4,8 +4,6 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         TestCase.__init__(self, name)
+
     def testMethod(self):
         self.wasRun = 1
-    def run(self):
-        method = getattr(self, self.name)
-        method()
