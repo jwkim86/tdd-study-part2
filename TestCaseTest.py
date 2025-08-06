832d30bcbbaa4c15afb27214b3fd497453b338fe
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-08-07 01:18:12 +0900

    Remove test setUp

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -2,10 +2,8 @@ from TestCase import TestCase
 from WasRun import WasRun
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
         self.test.run()
         assert ("setUp testMethod " == self.test.log)
 
