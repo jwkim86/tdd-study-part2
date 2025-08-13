80826a16af79ad0dcd7f7fb7b594daf6e54dc791
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-08-13 23:43:38 +0900

    Add testSuite test

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -24,6 +24,13 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert("1 run, 1 failed" == result.summary())
 
+    def testSuite(sel):
+        suite= TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result= suite.run()
+        assert("2 run, 1 failed" == result.summary())
+
 print(TestCaseTest("testTemplateMethod").run().summary())
 print(TestCaseTest("testResult").run().summary())
 print(TestCaseTest("testFailedResult").run().summary())
